036110c7465757a8f83c504fa85e9a6d394a69a8
Lasitha Priyankara <priyankara.lasitha@meta.co.jp>
2021-10-25 16:26:18 +0900

    DI implementation

---
+++ src/main/java/lasitha/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,20 @@
+package lasitha.springframework.sfgdi.controllers;
+
+import lasitha.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
---
+++ src/main/java/lasitha/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,6 @@
+package lasitha.springframework.sfgdi.services;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
